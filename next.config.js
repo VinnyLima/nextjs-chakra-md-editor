ee140e641f798b41b544045e0591df681e736e48
Paulo Vinicius F Lima <paulo.vinicius.f.l@live.com>
2023-08-07 22:41:35 -0300

    feat: add  layout do projeto

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+const removeImports = require('next-remove-imports');
+module.exports = removeImports()({
+  // ✅  options...
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader',
+    });
+    return config;
+  },
+});
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
